fb006d8fd13d4a5400ec4934da37ca6c8251ae44
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2019-11-18 21:19:51 +0000

    Implemented functionality to like and unlike posts.
    Likes are unique through both validation and database contraint so that a User can only like a post once.

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,7 +1,16 @@
 module PostHelper
+
   def avatar(user)
     email_digest = Digest::MD5.hexdigest user.email
     avatar_url = "https://api.adorable.io/avatars/150/#{email_digest}"
     image_tag avatar_url
+  end
+
+  def like_button(post)
+    if current_user.liked?(post)
+      link_to 'Unlike', unlike_post_path(post), method: :delete
+    else
+      link_to 'Like', like_post_path(post), method: :post
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_222117) do
+ActiveRecord::Schema.define(version: 2019_11_18_210318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2019_11_15_222117) do
   create_table "image_posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -71,5 +81,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_222117) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
