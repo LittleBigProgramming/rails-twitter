0c938eb5f8d643f23371350a39bf1f1f998c4fb2
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-01-14 08:28:48 +0000

    Moved the SQL statement for searching for hashtags from the controller to the Post model as a scope.
    This is to keep the logic out of the controller and allow reuse.

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,8 +2,6 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = params[:id]
-    @results = Post
-               .joins("LEFT JOIN text_posts ON content_type = 'TextPost' AND content_id = text_posts.id")
-               .where('text_posts.body LIKE ?', "%##{@hashtag}%")
+    @results = Post.search(term: "##{@hashtag}")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
   validates :user, presence: true
 
   delegate :username, to: :user
+
+  scope :search, lambda { |term:|
+    joins("LEFT JOIN text_posts ON content_type = 'TextPost' AND content_id = text_posts.id")
+      .where('text_posts.body LIKE ?', "%#{term}%")
+  }
 end
